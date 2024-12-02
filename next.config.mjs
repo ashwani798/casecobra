2ba8f24b74701fd542c509ea36e93dfd489f093a
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-12-02 23:56:31 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["utfs.io"],
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["utfs.io"],
+  },
+};
+
+export default nextConfig;
