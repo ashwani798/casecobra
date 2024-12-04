3fdc45809462b3fa99685b05f7b33d9a2e449d32
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-12-04 20:56:10 +0530

    Initial commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,86 +1,77 @@
 import { db } from "@/db";
 import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(req: Request) {
   try {
     const body = await req.text();
-    const signature = req.headers.get("stripe-signature");
+    const signature = headers().get("stripe-signature");
 
     if (!signature) {
       return new Response("Invalid signature", { status: 400 });
     }
 
-    if (!process.env.STRIPE_WEBHOOK_SECRET) {
-      throw new Error("Stripe webhook secret is not set");
-    }
-
     const event = stripe.webhooks.constructEvent(
       body,
       signature,
-      process.env.STRIPE_WEBHOOK_SECRET
+      process.env.STRIPE_WEBHOOK_SECRET!
     );
 
-    switch (event.type) {
-      case "checkout.session.completed": {
-        const session = event.data.object as Stripe.Checkout.Session;
+    if (event.type === "checkout.session.completed") {
+      if (!event.data.object.customer_details?.email) {
+        throw new Error("Missing user email");
+      }
 
-        if (!session.customer_details?.email) {
-          throw new Error("Missing user email");
-        }
+      const session = event.data.object as Stripe.Checkout.Session;
 
-        const { userId, orderId } = session.metadata || {
-          userId: null,
-          orderId: null,
-        };
+      const { userId, orderId } = session.metadata || {
+        userId: null,
+        orderId: null,
+      };
 
-        if (!userId || !orderId) {
-          throw new Error("Invalid request metadata");
-        }
+      if (!userId || !orderId) {
+        throw new Error("Invalid request metadata");
+      }
 
-        const billingAddress = session.customer_details?.address;
-        const shippingAddress = session.shipping_details?.address;
+      const billingAddress = session.customer_details!.address;
+      const shippingAddress = session.shipping_details!.address;
 
-        if (!billingAddress || !shippingAddress) {
-          throw new Error("Missing address details");
-        }
-
-        await db.order.update({
-          where: { id: orderId },
-          data: {
-            isPaid: true,
-            shippingAddress: {
-              create: {
-                name: session.customer_details.name!,
-                city: shippingAddress.city!,
-                country: shippingAddress.country!,
-                postalCode: shippingAddress.postal_code!,
-                street: shippingAddress.line1!,
-                state: shippingAddress.state || null,
-              },
-            },
-            billingAddress: {
-              create: {
-                name: session.customer_details.name!,
-                city: billingAddress.city!,
-                country: billingAddress.country!,
-                postalCode: billingAddress.postal_code!,
-                street: billingAddress.line1!,
-                state: billingAddress.state || null,
-              },
+      await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state,
             },
           },
-        });
-        break;
-      }
-      default:
-        console.warn(`Unhandled event type: ${event.type}`);
+          billingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state,
+            },
+          },
+        },
+      });
     }
 
     return NextResponse.json({ result: event, ok: true });
   } catch (err) {
-    console.error("Webhook Error:", err);
+    console.error(err);
+
     return NextResponse.json(
       { message: "Something went wrong", ok: false },
       { status: 500 }
--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -3,12 +3,3 @@ import { generateReactHelpers } from "@uploadthing/react";
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
-
-// import { OurFileRouter } from "@/app/api/uploadthing/core";
-// import {
-//   generateUploadButton,
-//   generateUploadDropzone,
-// } from "@uploadthing/react";
-
-// export const UploadButton = generateUploadButton<OurFileRouter>();
-// export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
