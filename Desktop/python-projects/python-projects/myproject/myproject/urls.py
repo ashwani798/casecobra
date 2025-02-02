dbc60ae8a72974fa7c885339189d69d529a4499e
Ashwani kumar pandey <99007418+ashwani798@users.noreply.github.com>
2025-02-03 01:06:09 +0530

    Delete Desktop/python-projects/python-projects/myproject directory

--- Desktop/python-projects/python-projects/myproject/myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin  # Import admin module
-from django.urls import path, include  # Import path and include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Include the app's URLs
-]
